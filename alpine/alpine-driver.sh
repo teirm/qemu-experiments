c26c51b4124acd1b5b8a10bb0a0521537ad16c73
teirm <tribunus.vii@gmail.com>
2017-05-27 19:36:54 -0400

    Added port redirection to setup script

--- alpine/alpine-driver.sh
+++ alpine/alpine-driver.sh
@@ -4,6 +4,7 @@
 # of alpine linux qemu images
 
 OPTION=$1
+PORT=$2
 
 function f_error {
     echo "Error occured during $1"
@@ -12,22 +13,32 @@ function f_error {
 }
 
 function f_usage {
-    echo "Usage: bash alpine-driver.sh [boot|create|clean]"
+    echo "Usage: bash alpine-driver.sh [boot|create <port>|clean] "
     exit 1;
 }
 
 function f_boot {
+   
+    if [[ $PORT == "" ]]; then
+        echo "No port number provided"
+        f_usage 
+    fi 
+
     echo "Going to boot alpine linux"
+    
     if [ ! -e alpine.qcow ]; then
         echo "File 'alpine.qcow' does not exist"
         exit 1
     fi
+    echo "Accessible on port $PORT"
     
     # not catching this error since this function creates a bash shell
-    qemu-system-x86_64 alpine.qcow -boot c -net nic -net user -m 256 -localtime
+    qemu-system-x86_64 alpine.qcow -boot c -net nic -net user -m 256 -localtime -redir tcp:$PORT::22
+
 }
 
 function f_create {
+    
     echo "Going to create alpine linux image with 8GB hdd"
     if [ -e alpine.qcow ]; then
         echo "File 'alpine.qcow' exists!"
@@ -66,7 +77,7 @@ function f_clean {
 }
 
 
-if [[ $1 == "" ]]; then
+if [[ $OPTION == "" || $PORT == "" ]]; then
     echo "No Option given"
     f_usage
 fi
